64c01b4be4c0ed1ad02c5323cf1a567dea4a7a92
菜猫子neko <815743831@qq.com>
2021-12-15 19:37:41 +0800

    fix: 修复配置ESLint对Jest校验时的错误配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     // ecmaVersion用来指定你想要使用的 ECMAScript 版本
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'import', 'jest'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
